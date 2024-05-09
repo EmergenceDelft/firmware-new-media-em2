7c2492cc6c39d6799aaaad3ce5f62649c0dda2fb
Paco <paco@pacos-mbp.kpn>
2024-05-09 12:06:03 +0200

    Added code for sensor and hello message

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,34 @@
 #include <Arduino.h>
 #include <ArduinoWebsockets.h>
 #include <WiFi.h>
+#include <ArduinoJson.h>
+#include <SRF05.h>
 
-const char* ssid = "Galaxy2"; //Enter SSID
-const char* password = "tiggyebun"; //Enter Password
-const char* websockets_server = "ws://192.168.208.44:3000/echo"; //server adress and port
+#define TRIGGER_PIN 4
+#define ECHO_PIN 16
+
+
+const char* ssid = "VlietlaanVlinders"; //Enter SSID
+const char* password = "Braktouba45"; //Enter Password
+const char* websockets_server = "ws://192.168.2.5:3000/echo"; //server adress and port
 
 using namespace websockets;
+WebsocketsClient client;
+SRF05 SRF(TRIGGER_PIN, ECHO_PIN);
+TaskHandle_t pollingTaskHandle;
+
+
 
 void onMessageCallback(WebsocketsMessage message) {
     Serial.print("Got Message: ");
     Serial.println(message.data());
 }
-
+ 
 void onEventsCallback(WebsocketsEvent event, String data) {
     if(event == WebsocketsEvent::ConnectionOpened) {
-        Serial.println("Connnection Opened");
+        Serial.println("Connection Opened");
     } else if(event == WebsocketsEvent::ConnectionClosed) {
-        Serial.println("Connnection Closed");
+        Serial.println("Connection Closed");
     } else if(event == WebsocketsEvent::GotPing) {
         Serial.println("Got a Ping!");
     } else if(event == WebsocketsEvent::GotPong) {
@@ -25,7 +36,24 @@ void onEventsCallback(WebsocketsEvent event, String data) {
     }
 }
 
-WebsocketsClient client;
+String getHelloMessage() {
+    JsonDocument doc;
+
+    doc["type"] = "hello";
+    doc["mac_address"] = WiFi.macAddress();
+
+    String serializedDoc;
+
+    serializeJson(doc, serializedDoc);
+    return serializedDoc;
+}
+
+void pollingTask( void * pvParameters ){
+    for(;;) {
+        client.poll();
+    }
+}
+
 void setup() {
     Serial.begin(115200);
     // Connect to wifi
@@ -47,11 +75,22 @@ void setup() {
     client.connect(websockets_server);
 
     // Send a message
-    client.send("Hi Server!");
+    client.send(getHelloMessage());
     // Send a ping
     client.ping();
+
+    //All polling code is executed on a different thread to keep the main thread open for reading sensor data.
+    xTaskCreatePinnedToCore(
+             pollingTask,  /* Task function. */
+             "Sensor task",    /* name of task. */
+             10000,      /* Stack size of task */
+             NULL,       /* parameter of the task */
+             1,          /* priority of the task */
+             &pollingTaskHandle,     /* Task handle to keep track of created task */
+             0);         /* pin task to core 0 */
 }
 
 void loop() {
-    client.poll();
+    Serial.println(SRF.getCentimeter(), 1);
+    delay(1000);
 }
