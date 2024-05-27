f5b0697c9b5d26412a9b73f2ab3dc3c63cc8c93b
Matei Cristea-Enache <mateicristeaenache88@gmail.com>
2024-05-27 11:34:14 +0200

    fixing semicolons in header file

--- include/main.h
+++ include/main.h
@@ -19,9 +19,9 @@
 #define SERVO_PWM_FREQUENCY 50.0f
 #define SERVO_MIN_PULSE_WIDTH 125 // Minimum pulse width for the servo
 #define SERVO_MAX_PULSE_WIDTH 550 // Maximum pulse width for the servo
-#define SSID "NPRouter"; //Enter SSID
-#define PASSWORD "keepitquantum";
-#define CONNECTION_STRING "ws://192.168.4.5:3000/echo";
+#define SSID "NPRouter" //Enter SSID
+#define PASSWORD "keepitquantum"
+#define CONNECTION_STRING "ws://192.168.4.4:3000/echo"
 
 
 
@@ -32,9 +32,6 @@ enum SensorType {
     CAPACITIVE
 };
 
-enum MessageType {
-    CAPACITIVE
-};
 
 using namespace websockets;
 
